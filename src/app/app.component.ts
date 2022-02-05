8038167938bf8ca5c5e2a9d865289873e319f6a8
Tamilselvi Rajesh <tamilselvi.rajesh@iinterchange.net>
2022-02-05 20:49:41 +0530

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from "@angular/common";
+interface PriceCard{
+  id : Number,
+  category : String,
+  userType : String,
+  amount : String,
+  Validity : String
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'priceCard';
+  title = 'priceCards';
+  priceCards:Array<PriceCard> = [
+  {
+    id : 1,
+    category : 'FREE',
+    userType : 'single user',
+    amount : '$0/month',
+    Validity : '15 days'
+  },
+  {
+    id : 2,
+    category : 'PLUS',
+    userType : '5 Users',
+    amount : '$9/month',
+    Validity : '1 Year'
+  },{
+    id : 3,
+    category : 'PRO',
+    userType : 'Unlimited Users',
+    amount : '$49/month',
+    Validity : '5 Years'
+  }
+]
+
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 
+interface PriceCard{
+  id : Number,
+  category : String,
+  userType : String,
+  amount : String,
+  Validity : String
+}
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
-  constructor() { }
+  @Input() priceCardData:PriceCard ={
+    id : 0,
+    category : '',
+    userType : '',
+    amount : '',
+    Validity : ''
+  }
 
   ngOnInit(): void {
   }
